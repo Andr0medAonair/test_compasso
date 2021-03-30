e358ce62a0bf8ef5bbce431c41530138c0c0bd9c
Rafaéla Jàneczko <rafaela.janeczko@compasso.com.br>
2021-03-30 09:41:07 -0300

    Implementando Swagger

--- database/client/client_Functions.js
+++ database/client/client_Functions.js
@@ -4,6 +4,76 @@ const Sequelize = require('sequelize')
 const moment = require('moment')
 
 module.exports ={
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Client:
+ *      type: "object"
+ *          required:
+ *              - "name"
+ *              - "gender"
+ *              - "birthday"
+ *              - "age"
+ *              - "city"
+ *          properties:
+ *              id:
+ *                  type: "integer"
+ *                  format: "int64"
+ *              name:
+ *                  type: "string"
+ *                  example: "Joan Fontaine"
+ *              gender:
+ *                  type: "string"
+ *                  enum:
+ *                      - "M"
+ *                      - "F"
+ *                      - "O"
+ *                  example: "F"
+ *              birthday:
+ *                  type: "string"
+ *                  example: "22/10/1917"
+ *              age:
+ *                  type: "integer"
+ *                  example: "96"
+ *              city:
+ *                  type: "string"
+ *                  format: "Tokyo"
+ *              creationDate:
+ *                  type: "string"
+ *                  example: "01/01/2001T13:49:24.647Z"
+ *              updateDate:
+ *                  type: "string"
+ *                  example: "01/01/2001T13:49:24.647Z"
+ *              version:
+ *                  type: "string"
+ *                  example: "1.0"
+ *              id:
+ *                  type: "integer"
+ *                  format: "int64"
+ */
+
+    /**
+  * @swagger
+  * tags:
+  *   name: Client
+  *   description: Client space
+  */
+
+    /**
+     * @swagger
+     * /clients:
+     *   get:
+     *     summary: Returns the list of all the books
+     *     tags: [Client]
+     *          responses:
+     *       "200":
+     *         description: Operation successful!
+     *         schema:
+     *           type: array
+     *           items:
+     *             $ref: "#/components/Client"
+     */
 //LIST CLIENTS
     list(){
         return model.findAll({ raw: true })
@@ -58,6 +128,27 @@ module.exports ={
         )
     },
 //DELETE REQUESTED CLIENT'S DATA
+    /**
+     * @swagger
+     * /{id}:
+     *   delete:
+     *     summary: Remove the client by id
+     *     tags: [Client]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         schema:
+     *              type: "integer"
+     *              format: "int64"
+     *         required: true
+     *         description: The book id
+     *     responses:
+     *       200:
+     *         description: Removed client {id} successfully!
+     *       404:
+     *         description: Client not found!
+     */
+
     remove(id){
         return model.destroy({
             where:{id:id},
